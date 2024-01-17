c47a4d299afba9235db4fd085a29a40065ce2dfe
Aditya Pathak <adityap029@gmail.com>
2024-01-16 23:57:31 -0500

    Commiting before I add the 2 event loop folders to this project

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ const _ = require("lodash");
 const items = [1, [2, [3, [4]]]];
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+console.log(
+  "We need a tackle at 6, but Brock Bowers Malik Nabers or Rome Odunze would work too"
+);
